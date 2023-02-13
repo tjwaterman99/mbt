26ac38fc2c4cecb32453746ac5f806499301f5b4
Tom Waterman <tjwaterman99@gmail.com>
2023-02-13 08:55:53 +0000

    Remove git service for now

--- mbt/services/git.py
+++
@@ -1,6 +0,0 @@
-from git import Git as GitPy
-
-
-class Git:
-    def __init__(self):
-        self._git = GitPy()
--- tests/services/test_git.py
+++
@@ -1,6 +0,0 @@
-from mbt.services.git import Git
-
-
-def test_git_init():
-    git = Git()
-    assert git is not None
